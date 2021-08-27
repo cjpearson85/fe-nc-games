d2be9c4fffcf64ee3d08ec9a2ef2fe9ad3e1cbf2
Christopher Pearson <cjpearson@hotmail.com>
2021-08-27 13:06:03 +0100

    add nav bar & styling

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import styles from '../css_modules/Nav.module.css';
+
+const Nav = () => {
+    return (
+        <nav>
+            <h1>^NC Games</h1>
+        </nav>
+    );
+};
+
+export default Nav;
