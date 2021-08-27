9c4e1e5274f05c1d25da75e95657e4a7faa99194
Christopher Pearson <cjpearson@hotmail.com>
2021-08-27 13:07:54 +0100

    base App layout and functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Nav from "./components/Nav";
+import ReviewList from "./components/ReviewList";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Nav />
+    <Switch>
+      <Route path="/">
+        <ReviewList />
+      </Route>
+    </Switch>
+  </div>;
 }
 
 export default App;
