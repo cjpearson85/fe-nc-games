c30013ddb2c0ed1104ebbdb6432b82b843afdf4c
Christopher Pearson <cjpearson@hotmail.com>
2021-09-02 12:11:42 +0100

    add tab icon burger icon to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,12 @@
 import styles from '../css_modules/Nav.module.css';
+import burger from '../images/18557012.png';
 
 const Nav = ({ sidebarOpen, setSidebarOpen }) => {
     return (
         <nav>
             <h1>^NC games</h1>
             {/* <h1 className={styles.burger_icon} onClick={() => sidebarOpen ? setSidebarOpen(false) : setSidebarOpen(true)}>=</h1> */}
-            <img src='https://cdn4.iconfinder.com/data/icons/circles-1/32/364-01-512.png' alt="burger_icon" className={styles.burger_icon} onClick={() => sidebarOpen ? setSidebarOpen(false) : setSidebarOpen(true)} />
+            <img src={burger} alt="burger_icon" className={styles.burger_icon} onClick={() => sidebarOpen ? setSidebarOpen(false) : setSidebarOpen(true)} />
         </nav>
     );
 };
