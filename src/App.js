59f893d764dfd943359c52e9b3132031d8b40383
Christopher Pearson <cjpearson@hotmail.com>
2021-10-12 22:46:29 +0100

    add new global state & props

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
       'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg'
   });
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false)
 
   return (
     <div className="App">
@@ -25,14 +26,21 @@ function App() {
         loggedInAs={loggedInAs}
         sidebarOpen={sidebarOpen}
         setSidebarOpen={setSidebarOpen}
+        searchOpen={searchOpen}
+        setSearchOpen={setSearchOpen}
       />
-      {sidebarOpen && (
+      {/* {sidebarOpen && (
         <Sidebar loggedInAs={loggedInAs} setLoggedInAs={setLoggedInAs} />
-      )}
+      )} */}
+      <Sidebar
+        loggedInAs={loggedInAs}
+        setLoggedInAs={setLoggedInAs}
+        sidebarOpen={sidebarOpen}
+      />
       <main>
         <Switch>
           <Route exact path="/">
-            <ReviewList loggedInAs={loggedInAs} />
+            <ReviewList loggedInAs={loggedInAs} searchOpen={searchOpen} />
           </Route>
           <Route exact path="/login">
             <Login setLoggedInAs={setLoggedInAs} />
@@ -47,10 +55,13 @@ function App() {
             <UsersList />
           </Route>
           <Route exact path="/users/:username">
-            <UserProfile loggedInAs={loggedInAs} setLoggedInAs={setLoggedInAs} />
+            <UserProfile
+              loggedInAs={loggedInAs}
+              setLoggedInAs={setLoggedInAs}
+            />
           </Route>
           <Route exact path="/categories/:category">
-            <ReviewList loggedInAs={loggedInAs} />
+            <ReviewList loggedInAs={loggedInAs} searchOpen={searchOpen} />
           </Route>
           <Route exact path="/reviews/:review_id">
             <FullReview loggedInAs={loggedInAs} />
@@ -62,7 +73,7 @@ function App() {
       </main>
       {/* <footer /> */}
     </div>
-  );
+  )
 }
 
 export default App;
