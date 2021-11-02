4d407f86a8351a96071ec6fb206edfabc95d7f74
Christopher Pearson <cjpearson@hotmail.com>
2021-11-02 20:01:34 +0000

    add extra container to restrict width on desktop screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,36 +6,38 @@ import search from '../images/icons8-search-24.png'
 const Nav = ({ sidebarOpen, setSidebarOpen, searchOpen, setSearchOpen }) => {
   return (
     <header>
-      <div
-        className={`${styles.icon_container} ${
-          sidebarOpen && styles.highlighted
-        }`}
-      >
-        <img
-          src={sidebarOpen ? cancel : burger}
-          alt={sidebarOpen ? 'cancel_icon' : 'burger_icon'}
-          className={sidebarOpen ? styles.cancel_icon : styles.burger_icon}
-          onClick={() => {
-            setSidebarOpen((curr) => !curr)
-            !sidebarOpen && setSearchOpen(false)
-          }}
-        />
-      </div>
-      <h1 className={styles.title}>^NC games</h1>
-      <div
-        className={`${styles.icon_container} ${
-          searchOpen && styles.highlighted
-        }`}
-      >
-        {/* <img
-          src={searchOpen ? cancel : search}
-          alt={searchOpen ? 'cancel_icon' : 'search_icon'}
-          className={searchOpen ? styles.cancel_icon : styles.search_icon}
-          onClick={() => {
-            setSearchOpen((curr) => !curr)
-            !searchOpen && setSidebarOpen(false)
-          }}
-        /> */}
+      <div className={styles.head_container}>
+        <div
+          className={`${styles.icon_container} ${
+            sidebarOpen && styles.highlighted
+          }`}
+        >
+          <img
+            src={sidebarOpen ? cancel : burger}
+            alt={sidebarOpen ? 'cancel_icon' : 'burger_icon'}
+            className={sidebarOpen ? styles.cancel_icon : styles.burger_icon}
+            onClick={() => {
+              setSidebarOpen((curr) => !curr)
+              !sidebarOpen && setSearchOpen(false)
+            }}
+          />
+        </div>
+        <h1 className={styles.title}>^NC games</h1>
+        <div
+          className={`${styles.icon_container} ${
+            searchOpen && styles.highlighted
+          }`}
+        >
+          {/* <img
+            src={searchOpen ? cancel : search}
+            alt={searchOpen ? 'cancel_icon' : 'search_icon'}
+            className={searchOpen ? styles.cancel_icon : styles.search_icon}
+            onClick={() => {
+              setSearchOpen((curr) => !curr)
+              !searchOpen && setSidebarOpen(false)
+            }}
+          /> */}
+        </div>
       </div>
     </header>
   )
