982baa64e79c17ba9598ebb81e251fc1cdc2c02c
Christopher Pearson <cjpearson@hotmail.com>
2021-11-03 22:56:04 +0000

    minor styling changes & refactor

--- src/components/FullReview.jsx
+++ src/components/FullReview.jsx
@@ -20,7 +20,6 @@ const FullReview = () => {
   const [review, setReview] = useState({})
   const [comments, setComments] = useState([])
   const [commentInput, setCommentInput] = useState('')
-  const [commentsOpen, setCommentsOpen] = useState(true)
   const [isLoading, setIsLoading] = useState(true)
   const { review_id } = useParams()
   const history = useHistory()
@@ -35,7 +34,7 @@ const FullReview = () => {
       .catch(() => {
         history.push('/')
       })
-  }, [review_id])
+  }, [review_id, history])
 
   useEffect(() => {
     getComments(review_id).then((comments) => {
@@ -56,24 +55,23 @@ const FullReview = () => {
   const deleteComment = ({ target: { value } }) => {
     deleteCommentById(value).then(() => {
       setComments((currentComments) => {
-        return currentComments.filter(({ comment_id }) => comment_id != value)
+        return currentComments.filter(({ comment_id }) => comment_id !== value)
       })
     })
-  }
-
-  const toggleComments = () => {
-    setCommentsOpen((currStatus) => !currStatus)
   }
 
   if (isLoading) return <Loader />
   return (
     <div className="FullReview">
-      <img src={review.review_img_url} alt="" />
+      <img className={styles.image} src={review.review_img_url} alt="" />
       <div className={styles.review_container}>
         <h2 className={styles.title}>{review.title}</h2>
-        <div className={styles.avatar_handle}>
+        <div
+          className={styles.avatar_handle}
+          onClick={() => history.push(`/users/${review.owner}`)}
+        >
           <p className={styles.text}>
-            by <strong>{review.owner}</strong>
+            <strong>{review.owner}</strong>
           </p>
           <img className={styles.avatar_img} src={review.avatar_url} alt="" />
         </div>
@@ -86,11 +84,6 @@ const FullReview = () => {
       </div>
       <h4 className={styles.comment_header}>Comments ({comments.length})</h4>
       <div className={styles.comment_container}>
-        {/* {review.comment_count === '0' ? null : (
-          <button onClick={toggleComments}>
-            {commentsOpen ? 'Hide comments' : 'Load comments'}
-          </button>
-        )} */}
         {username && (
           <form
             className={styles.comment_input}
@@ -108,46 +101,42 @@ const FullReview = () => {
             <button className={styles.send_button}>
               <img src={postIcon} alt="" />
             </button>
-            {/* <span> {commentInput.length ? commentInput.length : null}</span> */}
           </form>
         )}
-        {commentsOpen ? (
-          <>
-            <ul className="comments">
-              {comments.map(
-                ({ comment_id, author, body, votes, created_at }) => {
-                  return (
-                    <li key={comment_id} className={styles.comment_card}>
-                      <div className={styles.comment_rowOne}>
-                        <h4 className={styles.avatar_handle}>{author}</h4>
-                        {username === author && (
-                          <button
-                            className={styles.delete}
-                            value={comment_id}
-                            onClick={deleteComment}
-                          >
-                            ❌
-                          </button>
-                        )}
-                      </div>
-                      <p className={styles.date}>
-                        posted {getTimeSince(created_at)}
-                      </p>
-                      <h5 className={styles.comment_likes}>
-                        {votes} {votes === 1 ? 'like' : 'likes'}{' '}
-                        <LikeButton
-                          setComments={setComments}
-                          comment_id={comment_id}
-                        />
-                      </h5>
-                      <p>{body}</p>
-                    </li>
-                  )
-                }
-              )}
-            </ul>
-          </>
-        ) : null}
+        <ul className={styles.comments}>
+          {comments.map(({ comment_id, author, body, votes, created_at }) => {
+            return (
+              <li key={comment_id} className={styles.comment_card}>
+                <div className={styles.comment_rowOne}>
+                  <h4
+                    className={styles.avatar_handle}
+                    onClick={() => history.push(`/users/${author}`)}
+                  >
+                    {author}
+                  </h4>
+                  {username === author && (
+                    <button
+                      className={styles.delete}
+                      value={comment_id}
+                      onClick={deleteComment}
+                    >
+                      ❌
+                    </button>
+                  )}
+                </div>
+                <p className={styles.date}>posted {getTimeSince(created_at)}</p>
+                <h4 className={styles.comment_likes}>
+                  {votes} {votes === 1 ? 'like' : 'likes'}{' '}
+                  <LikeButton
+                    setComments={setComments}
+                    comment_id={comment_id}
+                  />
+                </h4>
+                <p>{body}</p>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </div>
   )
