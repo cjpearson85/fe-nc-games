7b4a3db08f43df3a9c16e31ef9aef7ce230984ab
Christopher Pearson <cjpearson@hotmail.com>
2021-11-03 22:55:10 +0000

    minor styling changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { SidebarStatusContext } from '../App'
 import styles from '../css_modules/Header.module.css'
 import burger from '../images/icons8-menu-24.png'
 import cancel from '../images/icons8-delete-24.png'
-import search from '../images/icons8-search-24.png'
+// import search from '../images/icons8-search-24.png'
 
 const Header = () => {
   const { sidebarOpen, setSidebarOpen, searchOpen, setSearchOpen } =
@@ -11,29 +11,28 @@ const Header = () => {
 
   return (
     <header className={styles.banner}>
-      <div className={styles.head_container}>
-        <div
-          className={`${styles.icon_container} ${
-            sidebarOpen && styles.highlighted
-          }`}
-        >
-          <img
-            src={sidebarOpen ? cancel : burger}
-            alt={sidebarOpen ? 'cancel_icon' : 'burger_icon'}
-            className={sidebarOpen ? styles.cancel_icon : styles.burger_icon}
-            onClick={() => {
-              setSidebarOpen((curr) => !curr)
-              !sidebarOpen && setSearchOpen(false)
-            }}
-          />
-        </div>
-        <h1 className={styles.title}>^NC games</h1>
-        <div
-          className={`${styles.icon_container} ${
-            searchOpen && styles.highlighted
-          }`}
-        >
-          {/* <img
+      <div
+        className={`${styles.icon_container} ${
+          sidebarOpen && styles.highlighted
+        }`}
+      >
+        <img
+          src={sidebarOpen ? cancel : burger}
+          alt={sidebarOpen ? 'cancel_icon' : 'burger_icon'}
+          className={styles.icon}
+          onClick={() => {
+            setSidebarOpen((curr) => !curr)
+            !sidebarOpen && setSearchOpen(false)
+          }}
+        />
+      </div>
+      <h1 className={styles.title}>^NC games</h1>
+      <div
+        className={`${styles.icon_container} ${
+          searchOpen && styles.highlighted
+        }`}
+      >
+        {/* <img
             src={searchOpen ? cancel : search}
             alt={searchOpen ? 'cancel_icon' : 'search_icon'}
             className={searchOpen ? styles.cancel_icon : styles.search_icon}
@@ -42,7 +41,6 @@ const Header = () => {
               !searchOpen && setSidebarOpen(false)
             }}
           /> */}
-        </div>
       </div>
     </header>
   )
