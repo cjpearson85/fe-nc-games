587f696cfec20b3c72e9e6ca3e78b7c7de0b3c76
Christopher Pearson <cjpearson@hotmail.com>
2021-11-03 22:54:16 +0000

    break out styling into css module & refactor logic

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,100 +1,79 @@
 import { useState } from "react";
 import { patchReviewById, patchCommentById } from "../api";
+import styles from '../css_modules/LikeButton.module.css'
 
 const LikeButton = ({ setReview, review_id, setComments, comment_id }) => {
-  const [liked, setLiked] = useState(false);
+  const [liked, setLiked] = useState(false)
 
   const updateReviewLikes = (value) => {
     setReview((curr) => {
-      const updatedReview = { ...curr };
-      updatedReview.votes = updatedReview.votes + +value;
-      return updatedReview;
-    });
+      const updatedReview = { ...curr }
+      updatedReview.votes = updatedReview.votes + +value
+      return updatedReview
+    })
     patchReviewById(review_id, +value).catch(() => {
       setReview((curr) => {
-        const updatedReview = { ...curr };
-        updatedReview.votes = updatedReview.votes - +value;
-        return updatedReview;
-      });
-    });
-  };
+        const updatedReview = { ...curr }
+        updatedReview.votes = updatedReview.votes - +value
+        return updatedReview
+      })
+    })
+  }
 
   const updateCommentLikes = (value) => {
     setComments((currentComments) => {
       return currentComments.map((comment) => {
         if (comment.comment_id === comment_id) {
-          const updatedComment = { ...comment };
-          updatedComment.votes = updatedComment.votes + +value;
-          return updatedComment;
+          const updatedComment = { ...comment }
+          updatedComment.votes = updatedComment.votes + +value
+          return updatedComment
         } else {
-          return comment;
+          return comment
         }
-      });
-    });
+      })
+    })
     patchCommentById(comment_id, +value).catch(() => {
       setComments((currentComments) => {
         return currentComments.map((comment) => {
           if (comment.comment_id === comment_id) {
-            const updatedComment = { ...comment };
-            updatedComment.votes = updatedComment.votes - +value;
-            return updatedComment;
+            const updatedComment = { ...comment }
+            updatedComment.votes = updatedComment.votes - +value
+            return updatedComment
           } else {
-            return comment;
+            return comment
           }
-        });
-      });
-    });
-  };
+        })
+      })
+    })
+  }
 
   if (review_id) {
-    return liked ? (
+    return (
       <button
-        className="LikeButton"
-        value="-1"
+        className={`${styles.LikeButton} ${liked ? styles.unliked : ''}`}
+        value={liked ? '-1' : '1'}
         onClick={({ target: { value } }) => {
-          setLiked(false);
-          updateReviewLikes(value);
+          setLiked((curr) => !curr)
+          updateReviewLikes(value)
         }}
       >
         ❤️
       </button>
-    ) : (
-      <button
-        className="LikeButton"
-        value="1"
-        onClick={({ target: { value } }) => {
-          setLiked(true);
-          updateReviewLikes(value);
-        }}
-      >
-        🤍
-      </button>
-    );
+    )
   } else if (comment_id) {
-    return liked ? (
+    return (
       <button
-        className="LikeButton"
-        value="-1"
+        className={`${styles.LikeButton} ${liked ? styles.unliked : ''}`}
+        value={liked ? '-1' : '1'}
         onClick={({ target: { value } }) => {
-          setLiked(false);
-          updateCommentLikes(value);
+          setLiked((curr) => !curr)
+          updateCommentLikes(value)
         }}
       >
         ❤️
       </button>
-    ) : (
-      <button
-        className="LikeButton"
-        value="1"
-        onClick={({ target: { value } }) => {
-          setLiked(true);
-          updateCommentLikes(value);
-        }}
-      >
-        🤍
-      </button>
-    );
+    )
   }
-};
+}
 
 export default LikeButton;
