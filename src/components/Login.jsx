ce8a35c6594ea4e55daeaa5be9194fc152733f6c
Christopher Pearson <cjpearson@hotmail.com>
2021-11-29 13:39:38 +0000

    update login to use mui text fields

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,18 +2,27 @@ import { useState, useContext } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import { loginUser } from '../api'
 import { AppUserContext } from '../App'
+import { TextField, Button, Tooltip } from '@mui/material'
+import styles from '../css_modules/Login.module.css'
 
 const Login = () => {
   const { setLoggedInAs } = useContext(AppUserContext)
-  const [usernameInput, setUsernameInput] = useState('')
-  const [passwordInput, setPasswordInput] = useState('')
+  const [inputs, setInputs] = useState({
+    username: '',
+    password: '',
+  })
   const [userNotFound, setUserNotFound] = useState(false)
   const history = useHistory()
+  console.log(inputs)
 
-  const attemptLogin = (event) => {
-    event.preventDefault()
+  const handleChange = ({ target: { name, value } }) => {
+    if (userNotFound) setUserNotFound(false)
+    setInputs((values) => ({ ...values, [name]: value }))
+  }
+
+  const handleSubmit = () => {
     localStorage.clear()
-    loginUser({ username: usernameInput, password: passwordInput })
+    loginUser(inputs)
       .then((user) => {
         setLoggedInAs(user)
         history.push('/')
@@ -24,36 +33,57 @@ const Login = () => {
   }
 
   return (
-    <div className="Login">
-      <h2>Log In</h2>
-      <form onSubmit={attemptLogin}>
-        <label>
-          Username:{' '}
-          <input
-            type="text"
-            required
-            value={usernameInput}
-            onChange={({ target: { value } }) => setUsernameInput(value)}
-          />
-        </label>
-        <label>
-          Password:{' '}
-          <input
-            type="password"
-            required
-            value={passwordInput}
-            onChange={({ target: { value } }) => setPasswordInput(value)}
-          />
-        </label>
-        <button type="submit">Submit</button>
-        {userNotFound ? (
-          <p className="error">Username or password incorrect</p>
-        ) : null}
+    <div className={styles.page}>
+      <h2 className={styles.heading}>Log In</h2>
+      <form className={styles.container}>
+        <TextField
+          fullWidth
+          error={userNotFound}
+          label="Username"
+          variant="outlined"
+          type="text"
+          name="username"
+          value={inputs.username}
+          onChange={handleChange}
+          required
+        />
+        <TextField
+          fullWidth
+          error={userNotFound}
+          label="Password"
+          variant="outlined"
+          type="password"
+          name="password"
+          helperText={userNotFound ? 'Username or password incorrect' : ' '}
+          value={inputs.password}
+          onChange={handleChange}
+          required
+          margin="normal"
+        />
+        <Tooltip
+          title={
+            inputs.username === '' || inputs.password === ''
+              ? 'Please fill out all the required fields.'
+              : ''
+          }
+        >
+          <span>
+            <Button
+              variant="contained"
+              disabled={
+                inputs.username === '' || inputs.password === '' || userNotFound
+              }
+              onClick={handleSubmit}
+            >
+              Log in
+            </Button>
+          </span>
+        </Tooltip>
+        <p className={styles.link}>
+          Don't have an account? Click here to{' '}
+          <Link to="/register">register</Link>.
+        </p>
       </form>
-      <p>
-        Don't have an account? Click here to{' '}
-        <Link to="/register">register</Link>.
-      </p>
     </div>
   )
 }
